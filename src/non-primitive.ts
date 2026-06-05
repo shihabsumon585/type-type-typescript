028df563e9dc95dafb78cf74c5ade4eaa0859aea
Md Shihab Sumon <shihabsumon585@gmail.com>
2026-06-05 14:55:07 +0600

    non_primitive Data type

---
+++ src/non-primitive.ts
@@ -0,0 +1,11 @@
+// array, object
+
+let bazarList: string[] = ["eggs", "milk", "sugar"];
+
+let mixedArr: (string | number)[] = ["eggs", 12, "milk", 1, "sugar", 2];
+
+// tuple
+let coOrdinates: [number, number] = [20, 30];
+let couple: [string, string] = ["Husband", "wife"];
+
+let nameAndRoll: [string, number] = ["Md Shihab Sumon", 86];
