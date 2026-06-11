9a416b268153f141d12f90204ba9a1410be456ab
shihabsumon585 <shihabsumon585@gmail.com>
2026-06-11 17:25:34 +0600

    key of constraint.ts

--- src/keyOfConstraintNew.ts
+++ src/keyOfConstraintNew.ts
@@ -13,3 +13,30 @@ type poorPeoplesVehicle = keyof RichPeoplesVehicle;
 
 
 const myVehicle : poorPeoplesVehicle = "plane"
+
+
+
+const user = {
+    id: 243,
+    name: "Md Shihab Sumon",
+    address: {
+        city: "Mirpur, Dhaka"
+    }
+}
+
+type User = {
+    id: number;
+    name: string;
+    address: {
+        city: string;
+    }
+}
+
+
+const getPropertyFromObj = <X>(obj: X, key: keyof X) => {
+    return obj[key];
+}
+
+const result = getPropertyFromObj(user, "name");
+
+console.log(result)
