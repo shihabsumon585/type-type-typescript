89f6308ef7d7e2d844da1e7625cb54acccb83773
shihabsumon585 <shihabsumon585@gmail.com>
2026-06-12 20:46:32 +0600

    conditional

--- src/conditional.ts
+++ src/conditional.ts
@@ -1,16 +1,17 @@
 // conditional type : je type conditionar er upor nirvorsheel
 
 type A = null;
+
 type B = undefined;
 
-type C = A extends null ? true : B extends undefined ? true : false; // that is conditional type
+type C = A extends null ? true : B extends undefined ? true : false;
 
-type RichPeoplesVehicle = {
+type RichVehicles = {
     bike: string;
     car: string;
     ship: string;
 }
 
-type CheckVehicle <T> = T extends keyof RichPeoplesVehicle  ? true : false;
+type CheckVehicle<T> = T extends keyof RichVehicles ? true : false;
 
-type HasBike = CheckVehicle<"bike">
+type HasBike = CheckVehicle<"bike">;
